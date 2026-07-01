cf6e0ae78e697ad9d648e04c571ed1eec0de7c6f
Giridharan.S <91829091+giridharan2006@users.noreply.github.com>
2026-07-01 19:42:00 +0530

    Solved LeetCode 520 - Detect Capital

---
+++ Easy/520_Detect_Capital.cpp
@@ -0,0 +1,33 @@
+/*Q1. Detect Capital
+Easy
+We define the usage of capitals in a word to be right when one of the following cases holds:
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+Example 1:
+Input: word = "USA"
+Output: true
+Example 2:
+Input: word = "FlaG"
+Output: false
+Constraints:
+1 <= word.length <= 100
+word consists of lowercase and uppercase English letters.*/
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int upper = 0;
+        for (char c : word) {
+            if (isupper(c))
+                upper++;
+        }
+        if (upper == word.length())      // ALL CAPS
+            return true;
+        if (upper == 0)                  // all lowercase
+            return true;
+        if (upper == 1 && isupper(word[0])) // Only first capital
+            return true;
+        return false;
+    }
+};
