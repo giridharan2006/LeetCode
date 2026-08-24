2f4465b04ea541b4423abcec33d87e6c8f184be8
Giridharan.S <91829091+giridharan2006@users.noreply.github.com>
2026-08-24 19:32:59 +0530

    Solve LeetCode 94 Binary Tree Inorder Traversal

---
+++ Easy/94_BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,44 @@
+/*94. Binary Tree Inorder Traversal
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Explanation:
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,2,6,5,7,1,3,9,8]
+Explanation:
+Example 3:
+Input: root = []
+Output: []
+Example 4:
+Input: root = [1]
+Output: [1]
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(vector<int>& n,TreeNode* root){
+        if(root == nullptr) return;
+        inorder(n,root->left);
+        n.push_back(root->val);
+        inorder(n,root->right);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        std::vector<int> arr;
+        inorder(arr,root);
+        return arr;
+    }
+};
