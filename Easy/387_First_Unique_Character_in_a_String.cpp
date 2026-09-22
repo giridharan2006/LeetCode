21e8129d1de980b13741d67d87bfa153e319ea6a
Giridharan.S <91829091+giridharan2006@users.noreply.github.com>
2026-09-22 18:54:45 +0530

    Create 387_First_Unique_Character_in_a_String.cpp

---
+++ Easy/387_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,31 @@
+/*387. First Unique Character in a String
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+Example 1:
+Input: s = "leetcode"
+Output: 0
+Explanation:
+The character 'l' at index 0 is the first character that does not occur at any other index.
+Example 2:
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+Input: s = "aabb"
+Output: -1
+Constraints:
+1 <= s.length <= 105
+s consists of only lowercase English letters.*/
+class Solution {
+    public int firstUniqChar(String s) {
+        char[] letters = s.toCharArray();
+        int freq[] = new int[26];
+        for(int i = 0;i<letters.length;i++){
+            freq[letters[i]-'a']++;
+        }
+        for(int i = 0;i<letters.length;i++){
+            if(freq[letters[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
